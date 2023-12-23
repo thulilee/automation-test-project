4768f74dbde0908a4dea0d0c97d479b63ad8de8e
Linh Le <lle6@slb.com>
2023-12-23 20:52:41 +0700

    test(api): update script

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: "src/UI/test/*.cy.ts",
+    specPattern: "src/ui/test/*.cy.ts",
     baseUrl: "https://en.wikipedia.org/wiki/Main_Page",
     supportFile: "src/UI/support/e2e.ts",
   },
--- src/API/testExecution.ts
+++ src/API/testExecution.ts
@@ -1,13 +1,42 @@
 import newman from "newman";
+import fs from "fs";
 
-newman.run({
-  collection: require("./collections/Get_wikipedia_collection_collection.json"),
-  iterationData: `${__dirname}/data/data.json`,
-  reporters: ["htmlextra", "cli"],
-  reporter: {
-    export: `${__dirname}/report/report.html`,
-    browserTitle: "Test report",
-    title: "Test Report",
-    displayProgressBar: true,
-  },
+let results: Array<boolean> = [];
+const listOfCollections: Array<string> = fs.readdirSync(
+  `${__dirname}/collections`
+);
+let executedCollectionCount: number = 0;
+
+listOfCollections.forEach((f) => {
+  newman
+    .run({
+      collection: require(`${__dirname}/collections/${f}`),
+      iterationData: `${__dirname}/data/${f.replace("collection", "data")}`,
+      reporters: ["htmlextra", "cli"],
+      reporter: {
+        browserTitle: "Test report",
+        title: "Test Report",
+        displayProgressBar: true,
+      },
+    })
+    .on("assertion", function (err) {
+      console.log(JSON.stringify(err));
+      if (err) {
+        results.push(false);
+      } else {
+        results.push(true);
+      }
+    })
+    .on("done", function () {
+      executedCollectionCount++;
+      if (
+        results.includes(false) &&
+        executedCollectionCount == listOfCollections.length
+      ) {
+        console.log(JSON.stringify(results) + "done with FAILED TEST");
+        process.exit(1);
+      } else {
+        console.log(JSON.stringify(results) + "done TEST");
+      }
+    });
 });
